a18fcf50b282733566bcc62f00a31f442f7a252a
Mujee125 <mujo6999@gmail.com>
2023-12-03 11:34:26 -0800

    Added PasswordInput component to show  EyeIcon and EyeOffIcon

--- create_business/OwnerInformation.tsx
+++ create_business/OwnerInformation.tsx
@@ -1,74 +1,73 @@
-
-import { Box} from '@gluestack-ui/themed';
-import FormInput from '../components/FormInput';
+import { Box } from "@gluestack-ui/themed";
+import FormInput from "../components/FormInput";
+import PasswordInput from "../components/PasswordInput";
 
 export default function OwnerInformation() {
   return (
     <Box p="$2" bg="$white">
       <FormInput
-        type={'text'}
+        type={"text"}
         isRequired={false}
-        label={'Prefix'}
-        placeholder={'Mr/ Mrs/ Miss'}
-        error={'Please specify your business name'}
+        label={"Prefix"}
+        placeholder={"Mr/ Mrs/ Miss"}
+        error={"Please specify your business name"}
         disabled={false}
         focus={null}
       />
       <FormInput
-        type={'text'}
+        type={"text"}
         isRequired={true}
-        label={'First Name'}
-        placeholder={'First Name'}
-        error={'Please specify your business name'}
+        label={"First Name"}
+        placeholder={"First Name"}
+        error={"Please specify your business name"}
         disabled={false}
         focus={null}
       />
       <FormInput
-        type={'text'}
+        type={"text"}
         isRequired={false}
-        label={'Last Name'}
-        placeholder={'Last Name'}
-        error={'Please specify your business name'}
+        label={"Last Name"}
+        placeholder={"Last Name"}
+        error={"Please specify your business name"}
         disabled={false}
         focus={null}
       />
       <FormInput
-        type={'text'}
+        type={"text"}
         isRequired={false}
-        label={'UserName'}
-        placeholder={'Usser Name'}
-        error={'Please specify your business name'}
+        label={"UserName"}
+        placeholder={"Usser Name"}
+        error={"Please specify your business name"}
         disabled={false}
         focus={null}
       />
       <FormInput
-        type={'text'}
+        type={"text"}
         isRequired={false}
-        label={'Email'}
-        placeholder={'Email'}
-        error={'Please specify your business name'}
+        label={"Email"}
+        placeholder={"Email"}
+        error={"Please specify your business name"}
         disabled={false}
         focus={null}
       />
-      <FormInput
-        type={'password'}
-        isRequired={true}
-        label={'Password'}
-        placeholder={'Password'}
-        error={'Please specify your business name'}
-        disabled={false}
-        focus={null}
-      />
-      <FormInput
-        type={'password'}
-        isRequired={true}
-        label={'Confirm Password'}
-        placeholder={'Confirm Password'}
-        error={'Please specify your business name'}
-        disabled={false}
-        focus={null}
-      />
-    </Box>
 
+      <PasswordInput
+        isRequired={true}
+        label={"Password"}
+        placeholder={"Password"}
+        error={"Please specify your business name"}
+        disabled={false}
+        focus={null}
+      />
+      <PasswordInput
+        isRequired={true}
+        label={"Confirm Password"}
+        placeholder={"Confirm Password"}
+        error={"Please specify your business name"}
+        disabled={false}
+        focus={null}
+      />
+      
+    </Box>
   );
-  }
+}
